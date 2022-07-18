f32da874d74cfbc5b5dee88eb8121b948dba46d5
Mr.LiuZJ <40499716+mm-man@users.noreply.github.com>
2022-07-18 22:07:53 +0800

    123

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -41,6 +41,9 @@ const website_list = reactive([
   ['https://www.bilibili.com/','哔哩哔哩','https://files.codelife.cc/website/bilibili2.svg'],
   ['http://v.qq.com/','腾讯视频','https://files.codelife.cc/website/qqvideo.svg'],
   ['https://vercel.com/','vercel','https://assets.vercel.com/image/upload/q_auto/front/favicon/vercel/favicon.ico'],
+  ['https://github.dev/mm-man/m-home-page','github.dev','https://assets.vercel.com/image/upload/q_auto/front/favicon/vercel/favicon.ico'],
+  
+  
 ])
 
 function websiteClick (site){
