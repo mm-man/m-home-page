831b2bbd2ac0d16fa51fac0ad5a21808f88ae9ef
Mr.LiuZJ <40499716+mm-man@users.noreply.github.com>
2022-07-18 23:09:34 +0800

    da'sdas123

--- src/data/website.js
+++ src/data/website.js
@@ -1,16 +1,19 @@
 export default [
     ['http://nodejs.cn/', 'nodejs', 'https://files.codelife.cc/website/5ab0785c7b072b4fee37ca12.png?x-oss-process=image/resize,limit_0,m_fill,w_100,h_100/quality,q_100'],
     ['https://gitee.com', 'gitee', 'https://files.codelife.cc/website/gitee.svg'],
-    ['https://github.com/','GitHub','https://files.codelife.cc/website/github.svg'],
+    ['https://github.com/','gitHub','https://files.codelife.cc/website/github.svg'],
+    ['https://github1s.com/mm-man/m-home-page','github1s','https://files.codelife.cc/website/github.svg'],
+    ['https://github.dev/mm-man/m-home-page','github.dev','https://files.codelife.cc/user-website-icon/20220718/GbhNibmJdnSjNyqXpTWLA6609.png?x-oss-process=image/resize,limit_0,m_fill,w_100,h_100/quality,q_100'],
     ['https://www.wolai.com/','我来','https://files.codelife.cc/website/www.wolai.com.svg'],
     ['https://www.bilibili.com/','哔哩哔哩','https://files.codelife.cc/website/bilibili2.svg'],
     ['http://v.qq.com/','腾讯视频','https://files.codelife.cc/website/qqvideo.svg'],
     ['https://vercel.com/','vercel','https://assets.vercel.com/image/upload/q_auto/front/favicon/vercel/57x57.png'],
-    ['https://github.dev/mm-man/m-home-page','github.dev','https://files.codelife.cc/user-website-icon/20220718/GbhNibmJdnSjNyqXpTWLA6609.png?x-oss-process=image/resize,limit_0,m_fill,w_100,h_100/quality,q_100'],
     ['https://fanyi.baidu.com','百度翻译','https://files.codelife.cc/website/fanyi-baidu.svg'],
     ['https://outlook.live.com/mail/0/inbox','Outlook','https://files.codelife.cc/website/outlook.svg'],
     ['https://mail.qq.com/','QQ邮箱','https://files.codelife.cc/website/qq-mail.svg'],
     ['https://element-plus.gitee.io/zh-CN/','element-plus','https://files.codelife.cc/website/elementui.svg'],
     ['https://element.eleme.cn/','element-ui','https://files.codelife.cc/website/elementui.svg'],
+  
 
+    
 ]
