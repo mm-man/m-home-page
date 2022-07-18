58532c5f6698235bd9fce7e1e1a3b4602e20a1fa
Mr.LiuZJ <40499716+mm-man@users.noreply.github.com>
2022-07-18 22:57:58 +0800

    da'sdas

--- src/data/website.js
+++ src/data/website.js
@@ -5,7 +5,7 @@ export default [
     ['https://www.wolai.com/','我来','https://files.codelife.cc/website/www.wolai.com.svg'],
     ['https://www.bilibili.com/','哔哩哔哩','https://files.codelife.cc/website/bilibili2.svg'],
     ['http://v.qq.com/','腾讯视频','https://files.codelife.cc/website/qqvideo.svg'],
-    ['https://vercel.com/','vercel','https://assets.vercel.com/image/upload/q_auto/front/favicon/vercel/favicon.ico'],
+    ['https://vercel.com/','vercel','https://assets.vercel.com/image/upload/q_auto/front/favicon/vercel/57x57.png'],
     ['https://github.dev/mm-man/m-home-page','github.dev','https://files.codelife.cc/user-website-icon/20220718/GbhNibmJdnSjNyqXpTWLA6609.png?x-oss-process=image/resize,limit_0,m_fill,w_100,h_100/quality,q_100'],
     ['https://fanyi.baidu.com','百度翻译','https://files.codelife.cc/website/fanyi-baidu.svg'],
     ['https://outlook.live.com/mail/0/inbox','Outlook','https://files.codelife.cc/website/outlook.svg'],
