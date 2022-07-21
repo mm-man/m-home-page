f5f865dd532f2951935581e56156620f81657424
liuzj <lzj470612639@outlook.com>
2022-07-21 10:27:27 +0800

    d

--- src/data/article.js
+++ src/data/article.js
@@ -1,10 +1,14 @@
 export default {
     life: [
-        
+
 
     ],
     work: [
-     
+        ['https://blog.csdn.net/IoTSchool/article/details/108896136','ThingsBoard 二次开发之源码分析 1- 基础知识'],
+        ['https://blog.csdn.net/IoTSchool/article/details/108896156','ThingsBoard 二次开发之源码分析 2-启动分析 1'],
+        ['https://blog.csdn.net/IoTSchool/article/details/108896173','ThingsBoard 二次开发之源码分析 3-启动分析 2'],
+        ['https://blog.csdn.net/IoTSchool/article/details/108896186','ThingsBoard 二次开发之源码分析 4-启动分析 3'],
+        ['https://blog.csdn.net/IoTSchool/article/details/108896193','ThingsBoard 二次开发之源码分析 5-如何接收 MQTT 连接'],
     ],
     code: [
         ['https://q.shanyue.tech/', '每日一题'],
