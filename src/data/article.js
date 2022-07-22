5146d78f1002ed6bce62297f52109998e79bee1d
Mr.LiuZJ <40499716+mm-man@users.noreply.github.com>
2022-07-22 09:28:51 +0800

    a

--- src/data/article.js
+++ src/data/article.js
@@ -9,6 +9,7 @@ export default {
         ['https://blog.csdn.net/IoTSchool/article/details/108896173','ThingsBoard 3-启动分析 2'],
         ['https://blog.csdn.net/IoTSchool/article/details/108896186','ThingsBoard 4-启动分析 3'],
         ['https://blog.csdn.net/IoTSchool/article/details/108896193','ThingsBoard 5-如何接收 MQTT 连接'],
+        ['http://doc.ruoyi.vip/ruoyi-vue/other/faq.html#前端静态资源如何整合到后端访问', '前端静态资源如何整合到后端访问']
     ],
     code: [
         ['https://q.shanyue.tech/', '每日一题'],
